d7b80655d82e0b4fd822051f9b5e5e8c16cf6138
SyedaQirrat <syedaqirratfatimazaidi2gmail.com>
2025-08-13 16:01:01 +0500

    aded more dummy data

--- app/page.tsx
+++ app/page.tsx
@@ -134,7 +134,7 @@ export default function BlogPage() {
             </div>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {filteredPosts.map((post) => (
               <PostCard
                 key={post.id}

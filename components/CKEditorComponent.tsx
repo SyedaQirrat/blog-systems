26d18fb30180b43cb91c71774274597798ec2be7
SyedaQirrat <syedaqirratfatimazaidi2gmail.com>
2025-08-18 17:51:17 +0500

    edited editor

--- components/CKEditorComponent.tsx
+++ components/CKEditorComponent.tsx
@@ -1,5 +1,5 @@
 // components/CKEditorComponent.tsx
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
@@ -9,36 +9,60 @@ interface CKEditorComponentProps {
 }
 
 const editorConfiguration = {
-    toolbar: [ 'bold', 
-						'|',  'italic', '|', 'undo', 
-						'|','redo',
-						'|', 'fontSize', '|',
-						'underline',
-						'strikethrough',
-						'subscript',
-						'code',
-						'|',
-						'removeFormat',
-						'|',
-						'link',
-						'|',
-						'highlight',
-						'bulletedList',
-						'|',
-						'numberedList',
-						'todoList' ]
+    toolbar: {
+        items: [
+            'undo',
+            'redo',
+            '|',
+            'heading',
+            '|',
+            'bold',
+            'italic',
+            'underline',
+            'strikethrough',
+            'subscript',
+            'code',
+            'removeFormat',
+            '|',
+            'link',
+            '|',
+            'bulletedList',
+            'numberedList',
+            'todoList',
+            '|',
+            'imageUpload',
+            'blockQuote'
+        ],
+    },
+    image: {
+        toolbar: [
+            'imageTextAlternative',
+            'toggleImageCaption',
+            'imageStyle:inline',
+            'imageStyle:block',
+            'imageStyle:side'
+        ]
+    },
+    list: {
+        properties: {
+            styles: true,
+            startIndex: true,
+            reversed: true
+        }
+    },
+    placeholder: 'Type or paste your content here!'
 };
 
 export const CKEditorComponent: React.FC<CKEditorComponentProps> = ({ value, onChange }) => {
     return (
         <CKEditor
-            editor={ ClassicEditor }
-            config={ editorConfiguration }
-            data={ value }
-            onChange={ ( event, editor ) => {
+            editor={ClassicEditor}
+            config={editorConfiguration}
+            data={value}
+            onChange={(event, editor) => {
                 const data = editor.getData();
                 onChange(data);
-            } }
+            }}
         />
     );
 };

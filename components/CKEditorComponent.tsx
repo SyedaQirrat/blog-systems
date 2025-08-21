b6f483070264a23eaea87992faa7f04966db1367
SyedaQirrat <syedaqirratfatimazaidi2gmail.com>
2025-08-21 17:23:35 +0500

    solved CK  errors

--- components/CKEditorComponent.tsx
+++ components/CKEditorComponent.tsx
@@ -1,20 +1,39 @@
 "use client";
 
 import React from "react";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
+import dynamic from "next/dynamic";
+import type { CKEditorProps } from "@ckeditor/ckeditor5-react";
+
+// Dynamically import CKEditor for SSR safety
+const CKEditor = dynamic(() => import("@ckeditor/ckeditor5-react").then(mod => mod.CKEditor), {
+  ssr: false,
+});
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-interface CKEditorComponentProps {
+interface Props {
   value: string;
-  onChange: (data: string) => void;
+  onChange: (value: string) => void;
 }
 
-export default function CKEditorComponent({ value, onChange }: CKEditorComponentProps) {
+export default function CKEditorComponent({ value, onChange }: Props) {
   return (
-    <CKEditor
-      editor={ClassicEditor}
-      data={value}
-      onChange={(_: unknown, editor: any) => onChange(editor.getData())}
-    />
+    <div style={{ marginTop: "10px" }}>
+      <CKEditor
+        editor={ClassicEditor}
+        data={value}
+        config={{
+          toolbar: [
+            "heading", "|",
+            "bold", "italic", "link", "|",
+            "bulletedList", "numberedList", "|",
+            "blockQuote", "insertTable", "undo", "redo"
+          ],
+        }}
+        onChange={(_, editor) => {
+          const data = editor.getData();
+          onChange(data);
+        }}
+      />
+    </div>
   );
 }
